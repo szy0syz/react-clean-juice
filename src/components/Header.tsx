06e9d77b0c83491a8806f6ebf10589a053ee55bf
szy0syz <szy0syz@gmail.com>
2022-01-06 12:07:45 +0800

    update: Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+const Header = () => {
+  return (
+    <nav className="header">
+      <span className="header-logo">CLEAN JUICE</span>
+      <ul>
+        <li>SHOP</li>
+        <li>STORY</li>
+        <li>MENU</li>
+        <li>CONTACT</li>
+        <li>ALL JUICES</li>
+      </ul>
+
+      <span className="header-account">My Account</span>
+      <span className="header-user"> 🙂 </span>
+    </nav>
+  );
+};
+
+export default Header;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
-import Delete from "@components/Delete";
+import Header from '@components/Header';
 
 export default function Home() {
-  return <Delete />;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
